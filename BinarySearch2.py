f1390c095757292d2e5febf4c44ce31394eb8ad2
RyanWeng40 <92039589+RyanWeng40@users.noreply.github.com>
2021-10-06 10:46:33 -0400

    Add files via upload

---
+++ BinarySearch2.py
@@ -0,0 +1,15 @@
+import pyinputplus as pyip
+
+list = [1, 2, 3, 4, 5, 6, 7, 8]
+key = pyip.inputInt("What would you like to find: ", min = 0, max = len(list))
+start = 0
+end = len(list) - 1
+mid = (end + start)//2
+while key != list[mid]:
+    mid = (end + start)//2
+    if list[mid] < key:
+        start = mid + 1
+    else:
+        end = mid - 1
+else:
+    print(f"{key} is at index {mid}.")
